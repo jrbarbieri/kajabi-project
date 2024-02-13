eaef1bb3968252972306c4f3b8a2c587ad78d181
Joao Barbieri <j.rafaeel@hotmail.com>
2024-02-13 18:49:41 -0300

    Refact service response

--- app/services/employees_list_service.rb
+++ app/services/employees_list_service.rb
@@ -4,6 +4,20 @@ class EmployeesListService
 
   def self.call
     response = get('/users')
-    response.parsed_response
+    build_response(response.parsed_response)
+  end
+
+  private
+
+  def self.build_response(response)
+    {
+      employee_list: response['data'],
+      pagination: {
+        page: response['page'],
+        per_page: response['per_page'],
+        total: response['total'],
+        total_pages: response['total_pages']
+      }
+    }.as_json
   end
 end
