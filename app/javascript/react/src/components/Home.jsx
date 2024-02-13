9361428e0e93f559ace88e21af796ef2f48f220b
Joao Barbieri <j.rafaeel@hotmail.com>
2024-02-13 13:04:38 -0300

    Create root path and its files

---
+++ app/javascript/react/src/components/Home.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react'                          
+                                                        
+const Hello = ({ name }) => {                                   
+  return (<div>Hello, { name }!</div>)                   
+}                                                       
+                                                                                                                                            
+export default Hello
--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -1,4 +1,4 @@
 import { define } from 'remount'      
-import Hello from "./components/Hello"
+import Home from "./components/Home"
                                       
-define({ 'hello-component': Hello })
+define({ 'home-component': Home })
